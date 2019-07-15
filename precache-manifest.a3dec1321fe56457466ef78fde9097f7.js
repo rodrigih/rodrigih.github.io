7a481abbe6cde0fd4bf007be41c4f2ab8a30a97e
Rodrigo Hernandez <rh3rn4nd3z@gmail.com>
2019-07-14 21:42:08 -0700

    Initial commit

---
+++ precache-manifest.a3dec1321fe56457466ef78fde9097f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "16c5ec21193d0799110484ae3673d12c",
+    "url": "/rodrigih/poke-project/static/media/pokeball.16c5ec21.png"
+  },
+  {
+    "revision": "c39e485f3cdc306882fc",
+    "url": "/rodrigih/poke-project/static/js/runtime~main.2e19fbdd.js"
+  },
+  {
+    "revision": "e7c899b41968c08e3605",
+    "url": "/rodrigih/poke-project/static/js/main.0ef2c547.chunk.js"
+  },
+  {
+    "revision": "25ff1386b2ff14a12c5c",
+    "url": "/rodrigih/poke-project/static/js/2.63f977b8.chunk.js"
+  },
+  {
+    "revision": "e7c899b41968c08e3605",
+    "url": "/rodrigih/poke-project/static/css/main.e4d8794b.chunk.css"
+  },
+  {
+    "revision": "d2f8f7699bc5c5d2e7b1d7b1360deebe",
+    "url": "/rodrigih/poke-project/index.html"
+  }
+];
